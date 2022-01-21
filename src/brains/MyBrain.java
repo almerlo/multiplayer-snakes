49a99a9cf25119f3687d3dc701b7647526aa5b18
Alan Merlo <alan.lmerlo@gmail.com>
2022-01-21 15:15:18 -0300

    Master ready

--- src/brains/MyBrain.java
+++ src/brains/MyBrain.java
@@ -8,37 +8,30 @@ import edu.unlam.snake.engine.Point;
 
 public class MyBrain extends Brain {
 
-	public MyBrain() {
-		super("IdLoom1 | IdLoom2 | IdLoom3");
-	}
+    public MyBrain() {
+        super("IdLoom1 | IdLoom2 | IdLoom3");
+    }
 
-	public Direction getDirection(Point head, Direction previous) {
-		//Frutas del mapa
-		List<Point> fruits = info.getFruits();
+    public Direction getDirection(Point head, Direction previous) {
+        List<Point> fruits = info.getFruits();
+        List<Point> snake = info.getSnake();
+        List<List<Point>> enemies = info.getEnemies();
+        List<Point> obstacles = info.getObstacles();
 
-		//Cuerpo de la serpiente
-		List<Point> snake = info.getSnake();
+        // Point head = snake.get(0);
+        Point myNextHead = head.clone();
+        myNextHead.moveTo(previous);
 
-		//Serpientes enemigas
-		List<List<Point>> enemies = info.getEnemies();
+        // Se pueden utilizar System.out para debuggear
+        // Deben ser quitados para la entrega
+        // System.out.println(myHead);
+        // System.out.println(myNextHead);
 
-		//Obstaculos del mapa
-		List<Point> obstacles = info.getObstacles();
+        for (Point obstacle : obstacles) {
+            if (myNextHead.equals(obstacle))
+                return previous.turnRight();
+        }
 
-		// Point head = snake.get(0);
-		Point myNextHead = head.clone();
-		myNextHead.moveTo(previous);
-
-		// Se pueden utilizar System.out para debuggear
-		// Deben ser quitados para la entrega
-		// System.out.println(myHead);
-		// System.out.println(myNextHead);
-
-		for (Point obstacle : obstacles) {
-			if (myNextHead.equals(obstacle))
-				return previous.turnRight();
-		}
-
-		return previous;
-	}
+        return previous;
+    }
 }
